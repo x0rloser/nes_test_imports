36b36cd586bc6220311b1fa9627ed247a2d2a920
xorloser <33712032+x0rloser@users.noreply.github.com>
2017-11-16 17:17:58 +1100

    added files

---
+++ nes_test_imports.sh
@@ -0,0 +1,22 @@
+if [[ $# -ne 1 ]]; then
+  echo "Usage: nes_test_imports.sh <kernel module filename>"
+  exit
+fi
+
+# get imported symbols from kernel module
+symbols=$(nm -u $1 | awk '{print $2}')
+
+# get directory that script is in so we can fine the symbols file in the same dir
+scriptdir=$(dirname "$BASH_SOURCE")
+
+# check if each symbol is exported by kernel
+for symbol in $symbols; do
+  # try to find symbol in list of symbols exported by kernel
+  result=$(grep -w $symbol $scriptdir/NES_mini_kernel_symbols-all.txt)
+  
+  # if symbol wasn't found, then tell us about it
+  if [[ -z $result ]]; then
+  	echo $symbol
+  fi
+done
+
